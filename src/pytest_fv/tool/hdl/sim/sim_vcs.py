087f47b9fec1ffc3e3be0764c8a0972963878b41
Matthew Ballance <matt.ballance@gmail.com>
2024-09-06 02:25:00 +0000

    XX
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- src/pytest_fv/tool/hdl/sim/sim_vcs.py
+++ src/pytest_fv/tool/hdl/sim/sim_vcs.py
@@ -39,8 +39,8 @@ class SimVCS(SimVlogBase):
         if not os.path.isabs(logfile):
             logfile = os.path.join(self.builddir, logfile)
 
-        if not os.path.isdir(os.dirname(logfile)):
-            os.makedirs(os.dirname(logfile))
+        if not os.path.isdir(os.path.dirname(logfile)):
+            os.makedirs(os.path.dirname(logfile))
             
         with open(logfile, "w") as log:
             pass
